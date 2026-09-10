40581e652214bfff8f4388679f7470fb61ced930
shauncaliso <ivocaliso0310@gmail.com>
2026-09-10 21:37:22 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    vite: {
+        server: {
+            watch: {
+                ignored: ['**/.vs/**']
+            }
+        }
+    }
+});
