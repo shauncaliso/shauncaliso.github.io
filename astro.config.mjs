469ea7e25dc2eb6716e08a073ecbd6a882288f78
shauncaliso <ivocaliso0310@gmail.com>
2026-09-19 01:06:42 +0800

    Preparation for real projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
-    vite: {
-        server: {
-            watch: {
-                ignored: ['**/.vs/**']
-            }
-        }
-    }
+  vite: {
+      server: {
+          watch: {
+              ignored: ['**/.vs/**']
+          }
+      }
+  },
+
+    site: "https://shauncaliso.github.io",
+    base: '/shauncaliso.github.io/',
+  integrations: [preact()]
 });
---
+++ src/pages/projects/project1.md
@@ -0,0 +1,11 @@
+---
+layout: 'C:/Users/Admin/portfolio/src/layouts/MarkdownPostLayout.astro'
+title: 'My First Project'
+pubDate: 2022-07-01
+description: 'This is my first project.'
+author: 'Shaun Caliso'
+image: 'C:/Users/Admin/Downloads/CALISO.png'
+tags: ["electronics", "project", "learning in public"]
+---
+
+This is my first project!
